d95bcd93ff6bf855c24a4bb7aa4c7f711b3f1690
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-11 21:12:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.naive_bayes import MultinomialNB
 
+from sklearn.pipeline import Pipeline
+clf=Pipeline([
+    ('vectorizer',CountVectorizer()),
+    ('nb',MultinomialNB())
+])
+
 
 
 
