ec9b4b0d1e46b2455c16043bf42c87b4dd60f9b3
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-11 21:12:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ data.isna().sum()
 data['Spam']=data['Category'].apply(lambda x:1 if x=='spam' else 0)
 data.head(5)
 
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(data.Message,data.Spam,test_size=0.25)
+
+
+
 
 
 
