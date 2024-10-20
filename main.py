90eaf557432c24654f74dc4e2b6c4dcc9f73e327
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-19 23:02:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ data=pd.read_csv('/kaggle/input/spam-email/spam.csv')
 data
 # we inlcude the columns
 data.column
-# and also shows the info of the 
+# and also shows the info of t
 data.info()
 
 data.isna().sum()
